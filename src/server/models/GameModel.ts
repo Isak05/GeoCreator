24bcb07624daebee3d310276faaa0137901a0082
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-05-08 12:43:21 +0200

    Add access control for editing maps

--- src/server/models/GameModel.ts
+++ src/server/models/GameModel.ts
@@ -5,60 +5,91 @@
  * @author Isak Johansson Weckstén <ij222pv@student.lnu.se>
  */
 
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import BASE_SCHEMA from "./baseSchema.js";
+import UserModel from "./UserModel.js";
+import { Request, Response } from "express";
 
-const vec2Schema = new mongoose.Schema({
-  x: {
-    type: Number,
-    required: true,
+const vec2Schema = new mongoose.Schema(
+  {
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    },
   },
-  y: {
-    type: Number,
-    required: true,
-  },
-}, {
-  _id: false,
-});
+  {
+    _id: false,
+  }
+);
 
-const screenshotSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Types.ObjectId,
-    auto: true,
+const screenshotSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.Types.ObjectId,
+      auto: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    correctAnswer: {
+      type: vec2Schema,
+      required: true,
+    },
   },
-  url: {
-    type: String,
-    required: true,
-  },
-  correctAnswer: {
-    type: vec2Schema,
-    required: true,
-  },
-}, {
-  _id: true,
-});
+  {
+    _id: true,
+  }
+);
 
-const schema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    mapUrl: {
+      type: String,
+      required: true,
+    },
+    screenshots: {
+      type: [screenshotSchema],
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: UserModel,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: false,
-  },
-  mapUrl: {
-    type: String,
-    required: true,
-  },
-  screenshots: {
-    type: [screenshotSchema],
-    required: true,
-  },
-}, {
-  _id: false,
-  id: false,
-});
+  {
+    _id: false,
+    id: false,
+    statics: {
+      checkIfAllowedToEdit(req: Request, res: Response, next: Function) {
+        if (
+          req.doc.creator?.toString() === undefined ||
+          req.session.loggedInUser?.id === undefined ||
+          req.doc.creator?.toString() !== req.session.loggedInUser.id
+        ) {
+          res.status(403).json({
+            message: "Forbidden",
+          });
+          return;
+        }
+        next();
+      },
+    },
+  }
+);
 
 schema.add(BASE_SCHEMA);
 
